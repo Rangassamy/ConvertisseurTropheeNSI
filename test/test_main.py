a34bee1fb58b5178b58c525511c3df33418beac1
Rangassamy <enzorangassamy@gmail.com>
2025-03-05 17:48:25 -0400

    rajout et supression des fichiers, manquant : vidéo

--- test/test_main.py
+++
@@ -1,9 +0,0 @@
-# Exemple de test unitaire
-import unittest
-
-class TestConversion(unittest.TestCase):
-    def test_conversion(self):
-        self.assertEqual(2 * 2, 4)
-
-if __name__ == '__main__':
-    unittest.main()
